129d92bf2623964bdf26959fc0bdb60449784fad
Jonathan <jonathan.clop@wanadoo.fr>
2018-04-12 15:02:27 +0200

    change label

--- resources/views/thematics.blade.php
+++ resources/views/thematics.blade.php
@@ -20,7 +20,7 @@
                            
                                 <span class="thematique_author">{{$user->identity}}</span> - <span class="thematique_date">Posté le {{$posts->date_creation}}</span><br>
                                 <span class="thematique_content">{{ $posts->content }}</span><br>
-                                <span class="thematique_last_message">Dernier message aujourd'hui à 18h48</span>
+                                <span class="thematique_last_message">Nouveau bouton Dernier message aujourd'hui à 18h48</span>
                             
                             </div>
                             <div class="thematique_arrow"><img src="../resources/images/picto_arrow.svg" class="picto" style="padding-right: -4px;"></div>
