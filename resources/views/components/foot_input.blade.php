eb6cb8527ddee3758ddca22a778baaae810c4552
Jonathan <jonathan.clop@wanadoo.fr>
2018-04-13 11:02:33 +0200

    Finition page thematics

--- resources/views/components/foot_input.blade.php
+++ resources/views/components/foot_input.blade.php
@@ -1,18 +1,20 @@
-<script type="text/javascript">
-    function asideswipe()   {
-        var div = document.getElementById('aside'),
-        display = getComputedStyle(div, null).display;
-         
-        if(display == "block")  {
-            document.getElementById("aside").style.display = "none";
-            document.getElementById("acontent").className = "col-md-12";
-        }else   {
-            document.getElementById("aside").style.display = "block";
-            document.getElementById("acontent").className = "col-md-9";
-        }
-    }  
-</script>
-
 <div class="footer">
-    <button class="navbar-toggle" onClick="asideswipe()">+</button> Copyright @ Forkid 2018
+    <div class="row">
+        <div class="col-md-3" style="text-align: center;">
+     Copyright @ Forkid 2018
+    </div>
+        <div class="col-md-9">
+            <form action="#">
+            <table style="width: 100%;">
+                <tr>
+                    <td style="width: 70%;">
+            	<input type="text" name="message" id="message">
+            	    </td>
+            	    <td style="padding-left: 10px;">
+            	<input border=0 src="../resources/images/picto_arrow3.svg" class="picto" id="okmessage" type=image Value="OK" align="top" > <a href="#"><img src="../resources/images/picto_smileys.svg" class="picto"></a>
+                    </td>
+                </tr>
+            </table>
+            </form>
+        </div>
 </div>
